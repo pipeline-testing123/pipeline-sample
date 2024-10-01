21a51b732c7a18bd312f63bbef8744b2b500fd79
Adam <40431677@live.napier.ac.uk>
2024-10-01 13:59:58 +0100

    Generated MAUI starter project
    Used Visual Studio's wonderful user interface to create a starting project for MAUI and .NET 8

---
+++ Software Engineering Pipeline Demo/Software Engineering Pipeline Demo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Software_Engineering_Pipeline_Demo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Software Engineering Pipeline Demo/Software Engineering Pipeline Demo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Software_Engineering_Pipeline_Demo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
