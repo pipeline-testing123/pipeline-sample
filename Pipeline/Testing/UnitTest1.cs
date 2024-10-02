c2341137a11285304d972c117891127040c8a63a
Adam <40431677@live.napier.ac.uk>
2024-10-02 20:08:08 +0100

    Renamed Project
    Project name was too long, caused some bugs where xUnit could not access a path because it was too long.

---
+++ Pipeline/Testing/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, 2 + 2);
+        }
+    }
+}
